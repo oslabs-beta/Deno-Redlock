71009c0d5db80a047698d4eb5696d09a872a12ee
cdschexnide <cody.schexnider@snhu.edu>
2022-05-10 19:44:07 -0500

    deps.ts

---
+++ deps.ts
@@ -0,0 +1,6 @@
+import EventEmitter from "https://deno.land/x/events@v1.0.0/mod.ts"
+import { connect } from "https://deno.land/x/redis@v0.25.5/experimental/cluster/mod.ts"
+
+
+
+export { EventEmitter, connect };
---
+++ index.ts
@@ -0,0 +1,1 @@
+ import { EventEmitter, connect } from './deps.ts';
