7af1163caa3a0fe61c774685b4a8b82aabb3c0a0
root <root@DESKTOP-58GIL04.localdomain>
2022-05-11 20:55:51 -0400

    complete redis type import testing

--- deps.ts
+++ deps.ts
@@ -1,6 +1,8 @@
 import EventEmitter from "https://deno.land/x/events@v1.0.0/mod.ts"
-import { connect } from "https://deno.land/x/redis@v0.25.5/experimental/cluster/mod.ts"
+import { connect } from "https://deno.land/x/redis@v0.25.5/mod.ts"
+import { RedisConnection } from "https://deno.land/x/redis@v0.25.5/connection.ts"
 
 
 
-export { EventEmitter, connect };
+
+export { EventEmitter, connect, RedisConnection };
--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
  import { EventEmitter, connect } from './deps.ts';
+
+
+ const redis = await connect({ hostname: "127.0.0.1", port: 6379 });
+
+//  await redis.clusterMeet("127.0.0.1", 6380);
